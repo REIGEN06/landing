072e4fdf6df5453c659d1b54f766f8ed1755af36
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-09 12:44:31 +0300

    feat: Все изменения перенесены из прошлого пр'а

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
+import Theme from './theme/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<Theme>
+			<Component {...pageProps} />
+		</Theme>
+	)
 }
