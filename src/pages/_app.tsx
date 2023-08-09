594e9a2ae56cf3dc41aaaa428ddf0ed1077782f9
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-09 23:53:28 +0300

    Revert "Merge branch 'dev' of https://github.com/REIGEN06/landing into dev"
    This reverts commit c60fcee95cc9ee3d5f0e0b1c729c1171a215aff8, reversing
    changes made to 1a2935734145cc5654d663cfd63b738af0e9073d.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,6 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import Theme from '../theme/theme'
-import NavBar from '../components/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Theme>
-			<NavBar />
-			{/* <Component {...pageProps} /> */}
-		</Theme>
-	)
+	return <Component {...pageProps} />
 }
