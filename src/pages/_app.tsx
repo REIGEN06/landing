0de23380ca6460a4f0d3ed8da0b18166b355a4bb
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-09 23:40:31 +0300

    feat: набросок навбара

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import Theme from './theme/theme'
+import Theme from '../theme/theme'
+import NavBar from '../components/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Theme>
-			<Component {...pageProps} />
+			<NavBar />
+			{/* <Component {...pageProps} /> */}
 		</Theme>
 	)
 }
