8a490547efe9147dd7a274ac9515b88b9b2ee949
George Aliev <gera08012329@gmail.com>
2023-08-10 09:37:19 +0300

    fix: delete examples from index.tsx and replace theme.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Typography, useTheme } from '@mui/material'
+import { Container, useTheme } from '@mui/material'
 import About from '@/components/about'
 
 const Landing = () => {
@@ -17,35 +17,6 @@ const Landing = () => {
 				maxWidth: '1330px',
 			}}
 		>
-			<Typography variant="h3">Landing Page</Typography>
-			<Container
-				maxWidth="md"
-				sx={{
-					p: 3,
-					marginTop: 3,
-					border: `1px solid ${theme.palette.border.main}`,
-					borderRadius: '10px',
-					backgroundColor: `${theme.palette.secondary.dark}`,
-				}}
-			>
-				<Typography variant="h4">Landing Page</Typography>
-			</Container>
-			<Container
-				maxWidth="md"
-				sx={{
-					p: 3,
-					marginTop: 3,
-					border: `1px solid ${theme.palette.border.main}`,
-					borderRadius: '10px',
-					backgroundColor: `${theme.palette.secondary.light}`,
-				}}
-			>
-				<Typography variant="h5">Landing Page</Typography>
-			</Container>
-			<Typography>Regular</Typography>
-			<Typography variant="regular">Regular too</Typography>
-			<Typography variant="medium">Medium</Typography>
-			<Typography variant="bold">Bold</Typography>
 			<About />
 		</Container>
 	)
