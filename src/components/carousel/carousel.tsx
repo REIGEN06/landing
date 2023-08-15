d1c41bf1bdc7a79b4986b76972c1e0f7d2710a14
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-15 11:14:33 +0300

    feat: навигация как в фигме

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,20 +1,38 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
-import { Stack, styled } from '@mui/material'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
+import { Button, IconButton, Stack, styled } from '@mui/material'
 import CarouselItem from './carouselItem'
+import ArrowLeft from '../../icons/carouselIcons/ArrowLeft.svg'
+import ArrowRight from '../../icons/carouselIcons/ArrowRight.svg'
 import { carouselArray, carouselItem } from './carouselArray'
+import { useRef } from 'react'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 const Carousel = () => {
+	const swiperRef = useRef<SwiperRef>(null)
+
+	const Prev = () => {
+		if (!swiperRef.current) return false
+
+		swiperRef.current.swiper.slidePrev()
+	}
+
+	const Next = () => {
+		if (!swiperRef.current) return false
+
+		swiperRef.current.swiper.slideNext()
+	}
+
 	return (
 		<StyledStack>
+			<IconButton onClick={Prev}>
+				<ArrowLeft />
+			</IconButton>
 			<StyledSwiper
-				modules={[Navigation]}
 				spaceBetween={0}
 				slidesPerView={1}
-				navigation
+				ref={swiperRef}
 				loop
 				breakpoints={{
 					768: {
@@ -28,6 +46,10 @@ const Carousel = () => {
 					</StyledSlide>
 				))}
 			</StyledSwiper>
+
+			<IconButton onClick={Next}>
+				<ArrowRight />
+			</IconButton>
 		</StyledStack>
 	)
 }
@@ -37,7 +59,8 @@ export default Carousel
 const StyledStack = styled(Stack)(({ theme }) => ({
 	background: `linear-gradient(180deg, ${theme.palette.secondary.light} 0%, ${theme.palette.secondary.dark} 100%)`,
 	padding: '80px',
-
+	flexDirection: 'row',
+	alignItems: 'center',
 	'@media (max-width: 1024px)': {
 		padding: '8px 20px',
 	},
@@ -45,6 +68,10 @@ const StyledStack = styled(Stack)(({ theme }) => ({
 
 const StyledSwiper = styled(Swiper)({
 	width: '100%',
+	'&.swiper-button-prev:after': {
+		color: 'red',
+		width: '666px',
+	},
 })
 
 const StyledSlide = styled(SwiperSlide)({
